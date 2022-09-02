48cc18a77468fa7cbdba066793ab9f5fa003b08c
ohjongchan <jongchan_oh@tmax.co.kr>
2022-09-02 11:20:21 +0900

    frontend setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
+import { ApolloProvider } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom";
+import { client } from "./apollo";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./styles/styles.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
