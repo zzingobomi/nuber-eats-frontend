646b4a74988740537f596ea21acee0fefed772d6
zzingobomi <zzingo5@gmail.com>
2022-09-02 23:31:38 +0900

    auth part.3

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import ReactDOM from "react-dom";
 import { client } from "./apollo";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { HelmetProvider } from "react-helmet-async";
 import "./styles/styles.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
